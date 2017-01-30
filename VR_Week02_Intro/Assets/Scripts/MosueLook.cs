763fd28bffca2c9174152b90c3bb68b8c3c3db2a
GordonCoffeeCan <664220842@qq.com>
2017-01-30 18:29:29 -0500

    laptop

---
+++ VR_Week02_Intro/Assets/Scripts/MosueLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//to access unity's VR functions, add the namespace;
+using UnityEngine.VR;
+
+public class MosueLook : MonoBehaviour {
+    private Transform _camTrans;
+
+	// Use this for initialization
+	void Start () {
+        _camTrans = Camera.main.transform;
+
+        if (VRDevice.isPresent == true) {
+            //if so, print vr HMD model name to the console;
+            Debug.Log("VR is enabled!" + VRDevice.model);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (VRDevice.isPresent == false) {
+            //do backup emergency input code only if VR isn't working
+            
+            //rotate camera based on mouse delta speed;
+            _camTrans.Rotate(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+
+            //unroll our camera
+            _camTrans.localEulerAngles = new Vector3(_camTrans.localEulerAngles.x, _camTrans.localEulerAngles.y, 0);
+        }
+	}
+}
